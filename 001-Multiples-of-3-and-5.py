50543bc0c92a6118ba62caa82c594bfb39cbea5e
Yuan Zhang <yuanzhang@Yuans-MacBook-Air.local>
2015-05-29 03:31:58 +0800

    001 bug fixed and add the basic algorithm

--- 001-Multiples-of-3-and-5.py
+++ 001-Multiples-of-3-and-5.py
@@ -1,8 +1,18 @@
+##############################################
+#Thinking 1: The basic idea, an O(n) algorithm
+res = 0
+for i in range(1000):
+    if i%3 == 0 or i%5==0:
+        res += i
+print(res)
+
+###################################
+#Thinking2: Using an O(1) algorithm
 def muiltipleNum( multifier , below ):
     res = (below-1) // multifier
     return res
 
 def sumOfMutiples( n , below ):
-    return ( n + muiltipleNum( n , 1000 )*n ) * muiltipleNum( n , 1000 ) // 2
+    return ( n + muiltipleNum( n , below )*n ) * muiltipleNum( n , below ) // 2
 
 print( sumOfMutiples( 3 , 1000) + sumOfMutiples( 5 , 1000) - sumOfMutiples( 15 , 1000 ) )
