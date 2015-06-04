b0d15e23de9b1164e9288767df4346b27aa1546f
liuyubobobo <liuyubobobo@gmail.com>
2015-06-04 22:42:20 +0800

    reorganize 001-003 code to compare different algorithms

--- 002-Even-Fibonacci-Numbers.py
+++ 002-Even-Fibonacci-Numbers.py
@@ -1,30 +1,49 @@
-fibs = [1,2]
+import time
 
-index = 2
-while True:
-    x = fibs[index-1] + fibs[index-2]
-    if x > 4000000:
-        break
-    fibs.append(x)
-    index = index + 1
-#print(fibs)
+if __name__ == "__main__":
 
-#Thinking 1: Judge even number by % operator
-print( sum( [x for x in fibs if x%2 == 0 ]) )
+    fibs = [1,2]
 
-#Thinking 2:
-#We can conclude that for every three number, the middle one is even.
-#To make things clear, we can add an extra 1 at the first
-#After that, we can conclude that every three number is even
-#Besides, we don't need to use the array to store all the fib numbers
+    index = 2
+    while True:
+        x = fibs[index-1] + fibs[index-2]
+        if x > 4000000:
+            break
+        fibs.append(x)
+        index = index + 1
+    #print(fibs)
 
-res = 0
-a = 1
-b = 1
-c = a+b
-while c <= 4000000:
-    res = res + c
-    a = b + c
-    b = a + c
-    c = a + b
-print(res)
+    #############################################
+    ##Thinking 1: Judge even number by % operator
+    #############################################
+    t1 = time.time()
+    print( sum( [x for x in fibs if x%2 == 0 ]) )
+    t2 = time.time()
+    print("Thinking 1 - time:",t2-t1,"s")
+
+    print("-"*50)
+
+    ## ------------------------------------------
+
+    #####################################################################
+    #Thinking 2:
+    #We can conclude that for every three number, the middle one is even.
+    #To make things clear, we can add an extra 1 at the first
+    #After that, we can conclude that every three number is even
+    #Besides, we don't need to use the array to store all the fib numbers
+    #####################################################################
+    t1 = time.time()
+    
+    res = 0
+    a = 1
+    b = 1
+    c = a+b
+    while c <= 4000000:
+        res = res + c
+        a = b + c
+        b = a + c
+        c = a + b
+    print(res)
+
+    t2 = time.time()
+    print("Thinking 1 - time:",t2-t1,"s")
--- 003-Largest-Prime-Factor.py
+++ 003-Largest-Prime-Factor.py
@@ -1,23 +1,25 @@
 from math import sqrt
 
-num = 600851475143
-
 def getRidOffFactor(num,factor):
     while num%factor==0:
         num = num/factor
     return num
 
-res = 1
-if num % 2 == 0:
-    res = 2
-    num = getRidOffFactor(num,2)
+if __name__ == "__main__":
 
-i = 3
-while i <= sqrt(num):
-    if num%i == 0:
-        res = max( res , i )
-        num = getRidOffFactor(num,i)
-    i = i + 2
+    num = 600851475143
+    res = 1
 
-res = max(res,num)
-print(res)
+    if num % 2 == 0:
+        res = 2
+        num = getRidOffFactor(num,2)
+
+    i = 3
+    while i*i <= num:
+        if num%i == 0:
+            res = max( res , i )
+            num = getRidOffFactor(num,i)
+        i = i + 2
+
+    res = max(res,num)
+    print(res)
