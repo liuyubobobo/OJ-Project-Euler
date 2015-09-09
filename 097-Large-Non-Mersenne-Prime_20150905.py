d1c7fab929f2d4b650524ede0d5d072ae62a4d41
liuyubobobo <liuyubobobo@gmail.com>
2015-09-09 22:03:04 +0800

    097 solved.

---
+++ 097-Large-Non-Mersenne-Prime_20150905.py
@@ -0,0 +1,55 @@
+import time
+
+
+def solve1( A , B , C , D , MOD ):
+
+    res = 1
+    for _ in range( C ):
+        res = ( res * B ) % MOD
+
+    return ( A * res + D ) % MOD
+
+
+def solve2( A , B , C , D , MOD ):
+
+    res = mypow( B , C , MOD )
+    return ( A * res + D ) % MOD
+
+
+def mypow( x , y , MOD ):
+
+    if y == 1:
+        return x
+
+    tres = mypow( x , y//2 , MOD )
+    if y%2 == 0:
+        return ( tres * tres ) % MOD
+    else:
+        return ( tres * tres * x ) % MOD
+
+    
+if __name__ == "__main__":
+
+    print( "solve 1" )
+    t1 = time.time()
+    print( solve1( 28433 , 2 , 7830457 , 1 , 10**10 ) )
+    t2 = time.time()
+    print( "time :" , t2 - t1 , "s" )
+    print()
+
+    print( "solve 2" )
+    t1 = time.time()
+    print( solve2( 28433 , 2 , 7830457 , 1 , 10**10 ) )
+    t2 = time.time()
+    print( "time :" , t2 - t1 , "s" )
+    print()
+
+    print( "solve 2" )
+    t1 = time.time()
+    print( solve2( 10**9 , 10**9 , 10**9 , 10**9 , 10**12 ) )
+    t2 = time.time()
+    print( "time :" , t2 - t1 , "s" )
+    print()
+
+    
+    
